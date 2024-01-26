a6b3bb2761efb400824c0594ee100c9b8bc2818b
felipegtrujillo <felipegtrujillo@gmail.com>
2024-01-26 16:51:12 -0300

    entrega

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,14 @@
+/* Tool tip para seccion Quienes somos */
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+// Espera a que el DOM esté listo
+    $(document).ready(function() {
+
+        // Agrega un evento clic al título "Quienes Somos"
+        $("#TituloQuienesSomos").on("click", function() {
+          alert("Hola ! Bienvenid@ a Viajes Chile !");
+        });
+
+   });
